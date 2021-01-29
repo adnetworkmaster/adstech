4c1cb0467d60311e6e50e6d407b29773f2518670
Raja <32867085+Raja1802@users.noreply.github.com>
2021-01-29 16:39:13 +0530

    Create javascript.js

---
+++ javascript.js
@@ -0,0 +1,8 @@
+(function (s, u, z, p) {
+  (s.src = u), s.setAttribute("data-zone", z), p.appendChild(s);
+})(
+  document.createElement("script"),
+  "https://iclickcdn.com/tag.min.js",
+  3899762,
+  document.body || document.documentElement
+);
