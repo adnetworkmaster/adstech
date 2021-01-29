2c951755f7ce35a742078402f124c0ac4a122b68
Raja <32867085+Raja1802@users.noreply.github.com>
2021-01-29 16:44:30 +0530

    Delete javascript.js

--- javascript.js
+++
@@ -1,8 +0,0 @@
-(function (s, u, z, p) {
-  (s.src = u), s.setAttribute("data-zone", z), p.appendChild(s);
-})(
-  document.createElement("script"),
-  "https://iclickcdn.com/tag.min.js",
-  3899762,
-  document.body || document.documentElement
-);
